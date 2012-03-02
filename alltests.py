a2606512eede58fcd3d5ec99a906790e6ed1b741
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-03-02 16:23:14 +0000

    comment out the keema test for now as the sample test doesn't support uri

--- alltests.py
+++ alltests.py
@@ -12,13 +12,13 @@ helper.insanity(
     ],
 )
 
-helper.insanity(
-    test = "blank-c-gst-test",
-    args = [
-      'pipeline-launch-line:constant:fakesrc num-buffers=1 ! fakesink',
-      'uri:filesystem:'+MEDIA_ROOT+'/srv/shared/media-samples'
-    ],
-)
+#helper.insanity(
+#    test = "blank-c-gst-test",
+#    args = [
+#      'pipeline-launch-line:constant:fakesrc num-buffers=1 ! fakesink',
+#      'uri:filesystem:'+MEDIA_ROOT+'/srv/shared/media-samples'
+#    ],
+#)
 
 helper.done()
 
