589c1619789d3b7e1bcbf4b63b308fa9b7b276ed
Vincent Penquerc'h <vincent.penquerch@collabora.co.uk>
2012-03-02 15:14:16 +0000

    Add a trawl through keema

--- alltests.py
+++ alltests.py
@@ -9,11 +9,13 @@ helper.insanity(
     args = [
       'pipeline-launch-line:constant:fakesrc num-buffers=1 ! fakesink'
     ],
-#  Hopefully something like:
-#    test = "discoverer",
-#    args = [
-#      'uri:filesystem:/mnt/keema/srv/shared/media-samples
-#    ],
+)
+
+helper.insanity(
+    test = "blank-c-gst-test",
+    args = [
+      'uri:filesystem:'+MEDIA_ROOT+'/srv/shared/media-samples'
+    ],
 )
 
 helper.done()
