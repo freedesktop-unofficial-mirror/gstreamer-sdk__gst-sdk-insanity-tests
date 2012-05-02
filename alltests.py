d383d85361c50a61590b226b63bb810ddb13cd5a
Alvaro Soliverez <alvaro.soliverez@collabora.co.uk>
2012-05-02 10:38:28 -0300

    Added a test for insanity-test-gst-play and removed dummy one

--- alltests.py
+++ alltests.py
@@ -6,9 +6,9 @@ import helper
 MEDIA_ROOT = helper.set_media_root("keema.collabora.co.uk:/")
 
 helper.insanity(
-    test = "blank-c-gst-test",
+    test = "insanity-test-gst-play",
     args = [
-      'pipeline-launch-line:constant:fakesrc num-buffers=1 ! fakesink'
+      'uri:filesystem:'+MEDIA_ROOT+'/srv/shared/media-samples/sorted/WebM'
     ],
 )
 
