3fde1c45fb25b72c17548d2486ab014960e9916e
Nicolas Dufresne <nicolas.dufresne@collabora.com>
2012-06-01 17:16:21 -0400

    Ensure done is called whatever happens

--- alltests.py
+++ alltests.py
@@ -5,20 +5,21 @@ import helper
 
 MEDIA_ROOT = helper.set_media_root("keema.collabora.co.uk:/")
 
-helper.insanity(
-    test = "play-test",
-    args = [
-      'uri:urifilesystem:'+MEDIA_ROOT+'/srv/shared/media-samples/sorted/WebM'
-    ],
-)
+try:
+    helper.insanity(
+        test = "play-test",
+        args = [
+            'uri:urifilesystem:'+MEDIA_ROOT+'/srv/shared/media-samples/sorted/WebM'
+        ],
+    )
 
-#helper.insanity(
-#    test = "blank-c-gst-test",
-#    args = [
-#      'pipeline-launch-line:constant:fakesrc num-buffers=1 ! fakesink',
-#      'uri:filesystem:'+MEDIA_ROOT+'/srv/shared/media-samples'
-#    ],
-#)
-
-helper.done()
+    #helper.insanity(
+    #    test = "blank-c-gst-test",
+    #    args = [
+    #      'pipeline-launch-line:constant:fakesrc num-buffers=1 ! fakesink',
+    #      'uri:filesystem:'+MEDIA_ROOT+'/srv/shared/media-samples'
+    #    ],
+    #)
+finally:
+    helper.done()
 
